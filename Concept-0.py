ad6bc3f64d4a85f9d831cdfa0fb184e71e1c8025
Cami Diez <diezcami@gmail.com>
2015-10-01 22:06:08 +0800

    Added sample input, updated test code

--- Concept-0.py
+++ Concept-0.py
@@ -1,37 +1,43 @@
 # File Handling
 source_code = open('source_code.txt')
-cum_qpi_data = source_code.readlines()[29]
+data_lines = source_code.readlines()
+cum_qpi_data = data_lines[29]
 cum_qpi = float(cum_qpi_data[16:])
-course_list = source_code.readlines()[32:]
+course_list = data_lines[32:]
+source_code.close()
 
-grade_lookup = {'A' : 4.0, 'B+': 3.5, 'B' : 3, 'C+': 2.5. 'C' : 2, 'D' : 1.5, 'F' : 0}
+grade_lookup = {'A' : 4.0, 'B+': 3.5, 'B' : 3, 'C+': 2.5, 'C' : 2, 'D' : 1.5, 'F' : 0}
 
 # Parse Data
 academic_units_taken = 0
-grade_total = 0
-last_in = len(course)-1
-units_in = 1
-
+grade_total = 0.0
+invalid_tags = [' PE ', 'NSTP']
+invalid_grades = ['W', 'S', 'UD']
 for course in course_list:
-
-    # Base Loop Case
     if not course.startswith('20'):
         break
 
-    # Filter ITM
-    if [' PE ', 'NSTP'] in course or
-        ['  W', '  S', 'AUD'] in course [-3:]
+    # TODO: Filter ITM
+    if any(tag in course for tag in invalid_tags):
         continue
     
-    str.strip('\s+')
+    course = course.replace(" ", "")
+    last_in = len(course) - 2 # One whitespace to the right
+    units_in = 1
+
+    if any(grade in course[last_in - 1:] for grade in invalid_grades):
+        continue
+
     if course[last_in] == '+':
-        units_index = 2
-    else:
-        units_index = 1
+        units_in = 2
     
     # Parse Course Information
-    grade_total += coursegrade_lookup[course[last_in]] * course[last_in - units_in]
-    academic_units_taken += int(course[last_in - units_in])
+    print course
+    course_units = int(course[last_in - units_in])
+    grade_total += (grade_lookup[course[last_in - units_in + 1 : last_in + 1]] * course_units)
+    academic_units_taken += course_units
+    print grade_lookup[course[last_in - units_in + 1 : last_in + 1]]
+
 
 current_cumulative_qpi = grade_total / academic_units_taken
 print (current_cumulative_qpi)
